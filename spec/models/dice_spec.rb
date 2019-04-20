113702806e417921a06d7981dd57ca830dca9096
MackHatfield <therealbakoonzar@gmail.com>
2019-04-20 15:55:59 -0700

    add test to ensure always within range

--- spec/models/dice_spec.rb
+++ spec/models/dice_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe Dice do
     it "returns a value less than or equal to 12 or 2 or greater" do
       expect(Dice.roll).to be_between(2, 12)
     end
+
+    it "always returns a value within the correct range" do
+      rolls = []
+      1000.times { rolls << Dice.roll }
+      expect(rolls).to all(be_between(2, 12))
+    end
+
   end
+
 end
