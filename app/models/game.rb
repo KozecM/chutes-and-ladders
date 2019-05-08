a5d467df6eabb4706e95fdda39c9bbd0aab78a66
Bryce Graves <brycegraves97@gmail.com>
2019-05-08 10:58:33 -0700

    Add todo to game

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,7 +11,7 @@ class Game < ApplicationRecord
     player_names = players.collect {|player| player.name }
     player_index = player_names.find_index(current_name)
     player_index += 1
-    @currently_rolling = players[player_index % players.size]
+    @currently_rolling = players[player_index % players.size] # TODO: flip these
   end
 
   def roll_for(player_name)
