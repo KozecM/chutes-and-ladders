ba2ad68942e1713eaa69bc20ce06048a52ee038a
Mattbar <mlbarnes04@gmail.com>
2019-06-06 22:24:29 -0700

    ADD: fail to add player no game id test, Fix logic in app to handle game_id requirment

--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -2,26 +2,31 @@ FactoryBot.define do
   factory :player, class: Player do
     name { "FAKE PLAYER" }
     position { 1 }
+    game_id { 1 }
   end
 
   factory :player2, class: Player do
     name { "FAKE PLAYER 2"}
     position { 1 }
+    game_id { 1 }
   end
 
   factory :player_pos_10, class: Player do
     name { "FAKE PLAYER_10"}
     position {10}
+    game_id { 1 }
   end
 
   factory :nameless_player, class: Player do
     name { "" }
     position { 1 }
+    game_id { 1 }
   end
 
   factory :nil_player, class: Player do
     name { nil }
     position { nil }
+    game_id { 1 }
   end
 
 end
--- spec/system/add_player_spec.rb
+++ spec/system/add_player_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "adding a player", type: :system do
   it "allows a user to add a player to the game" do
     visit new_player_path
     fill_in "Name", with: "FakeMack"
+    fill_in "Game", with: 2
     click_on("Create Player")
     visit players_path
     expect(page).to have_content("FakeMack")
@@ -41,5 +42,18 @@ RSpec.describe "adding a player fail", type: :system do
     visit new_player_path
     click_on("Create Player")
     expect(page).to have_content(`:name, ["can't be blank"]`)
+  end
+
+  it "displays game id error when game_id is blank" do
+    visit new_player_path
+    fill_in "Name", with: "FakeName"
+    click_on("Create Player")
+  end
+
+    it "displays game id error when game_id is not an int" do
+    visit new_player_path
+    fill_in "Name", with: "FakeName"
+    fill_in "Game", with: "e"
+    click_on("Create Player")
   end
 end
