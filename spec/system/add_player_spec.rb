9678c549bebc8354914b6f27d34c6152f6013c3e
Mattbar <mlbarnes04@gmail.com>
2019-06-06 21:34:03 -0700

    add system test to add_player_flow

--- spec/system/add_player_spec.rb
+++ spec/system/add_player_spec.rb
@@ -3,9 +3,27 @@ require "rails_helper"
 RSpec.describe "adding a player", type: :system do
   it "allows a user to add a player to the game" do
     visit new_player_path
-    fill_in "Name", with: "Mack"
+    fill_in "Name", with: "FakeMack"
     click_on("Create Player")
     visit players_path
-    expect(page).to have_content("Mack")
+    expect(page).to have_content("FakeMack")
+  end
+
+  it "allows a user to choose game to add player to" do
+    visit new_player_path
+    fill_in "Name", with: "FakeName"
+    fill_in "Game", with: 2
+    click_on("Create Player")
+    visit players_path
+    expect(page).to have_content("2")
+  end
+
+  it "sets players position to 1" do
+    visit new_player_path
+    fill_in "Name", with: "FakeName"
+    fill_in "Game", with: 2
+    click_on("Create Player")
+    visit players_path
+    expect(page).to have_content("1")
   end
 end
